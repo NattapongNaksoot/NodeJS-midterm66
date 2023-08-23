128d31ad1e4da4120535c9922db1fcde94d377ba
NattapongNaksoot <139940383+NattapongNaksoot@users.noreply.github.com>
2023-08-23 13:57:44 +0700

    test

--- server/src/que2.js
+++ server/src/que2.js
@@ -1,4 +1,3 @@
-
 let express = require('express')
 let bodyParser = require('body-parser')    
 
@@ -10,8 +9,6 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.get('/addnum/:addnumber', function (req, res) {
     res.send('ผลลัพธ์ = ' + req.params.addnumber / 2 ) 
 })
-
-
 
 let port = 8081
 app.listen(port, function () {
